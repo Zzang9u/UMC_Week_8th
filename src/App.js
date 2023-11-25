84ceebcf07a5fa1071176096403c15b45a255110
Zzang9u <146842085+Zzang9u@users.noreply.github.com>
2023-11-25 11:50:24 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,117 +1,100 @@
-import React,{ useState } from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
-function WeatherApp() {
-    const [cityname, setCityName] = useState("");
-    const [weatherData, setWeatherData] = useState();
-
-    const handleCityName = (event) => {
-        setCityName(event.target.value);
-    }
-    
-    const getWeatherData = async () => {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=7bf0ebd091ab052802bdb2ee70aaee45`;
-      try{
-        const response = await fetch(url);
-        const data = await response.json();
-        if(data.cod === 200){
-          setWeatherData(data);
-          console.log(weatherData);
-        }
-        else if(data.cod === "404") {
-          setWeatherData(data);
-          console.log(weatherData);
-        }
-      } catch (error) {
-        setWeatherData(null);
-        console.log(weatherData);
-        console.error('Err...:', error);
-      }
-    };
-
-    const handleKeyPress = (event) => {
-      if(event.key === "Enter"){
-        getWeatherData();
-      }
-    }
+function LoadingModal({ express }) {
+  if (!express) {
+    return null;
+  }
 
   return (
     <div
-      className='App'
       style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
         display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
         alignItems: "center",
-        width: "100vw",
-        height: "100vh",
+        justifyContent: "center",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
       }}
     >
-      <input
-      type='text'
-      placeholder='도시를 입력하세요'
-      value={cityname}
-      onChange={handleCityName}
-      onKeyDown={handleKeyPress}
-      style={{
-        width: "13vw",
-        height: "3vw",
-        border: "solid",
-        borderRadius: "17px",
-        paddingLeft: "20px"
-      }}
-      />
-      {weatherData ? (
-        <div
-          style={{
-            width: "15%",
-            height: "230px",
-            border: "3px solid",
-            borderRadius: "8px",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            padding: "20px",
-            boxSizing: "border-box",
-            marginTop: "70px"
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              width: "100%",
-              justifyContent: "flex-start",
-              fontSize: "30px"
-            }}
-          >
-            {weatherData.name}
-          </div>
-          <br />
-          <div
-            style={{
-              display: "flex",
-              width: "100%",
-              justifyContent: "flex-start",
-              fontSize: "75px",
-            }}
-          >
-            {Math.round((weatherData.main.temp - 273.15) * 10) / 10}°C
-          </div>
-          <br />
-          <div
-            style={{
-              display: "flex",
-              width: "100%",
-              justifyContent: "flex-end",
-              fontSize: "23px",
-            }}
-          >
-            {weatherData.weather[0].main}
-          </div>
-        </div>
-      ) : null}
+      <div
+        style={{
+          padding: "30px",
+          backgroundColor: "white",
+          borderRadius: "10px",
+        }}
+      >
+        Loading...
+      </div>
     </div>
   );
 }
 
-export default WeatherApp;
+function App() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    if(name === "id"){
+      setId(value);
+    }else if(name === "password"){
+      setPassword(value);
+    }
+  };
+
+  const handleClick = (e) => {
+    if (id === "" || password === "") {
+      alert("이벤트가 중단되었습니다.");
+      e.preventDefault();
+    }
+
+    setIsLoading(true);
+    
+    const response = axios.post("http://localhost:8000/user/login", {
+      id: id,
+      pw: password,
+    });
+    try {
+      console.log("data");
+      console.log(response.data.id);
+      console.log(response.data.pw);
+
+    } catch (e) {
+      console.log("error");
+      console.log(e);
+    }finally{
+      setTimeout(() => setIsLoading(false), 1500);
+    }
+
+    const data = response.data;
+
+    if(response.data.id != "umcweb"){
+      data.cod = "401";
+      setId(data);
+      console.log(id);
+    }
+    if(response.data.pw != "1234"){
+      data.cod = "402";
+      setPassword(data);
+      console.log(password);
+    }
+
+  };
+
+  return (
+    <div>
+      <input name="id" value={id} onChange={handleChange} />
+      <input name="password" value={password} type="password" onChange={handleChange}/>
+      <input type="submit" value={"로그인"} onClick={handleClick}/>
+      <LoadingModal express={isLoading} />
+    </div>
+  );
+}
+
+export default App;
